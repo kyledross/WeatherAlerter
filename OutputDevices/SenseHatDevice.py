a62507b1d8be25058fbc15f00b0758fade735ea9
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2024-06-16 22:53:35 -0400

    Implementing output choices

--- OutputDevices/SenseHatDevice.py
+++ OutputDevices/SenseHatDevice.py
@@ -4,18 +4,17 @@ from OutputDevices.BaseOutputDevice import BaseOutputDevice
 
 
 class SenseHatDevice(BaseOutputDevice):
-    _is_raspberry_pi = False
-
-    try:
-        sense_hat = importlib.import_module('sense_hat')
-        _is_raspberry_pi = True
-    except ModuleNotFoundError:
-        sense_hat = importlib.import_module('sense_emu')
-
-    sense = sense_hat.SenseHat()
-    if _is_raspberry_pi:
-        sense.set_rotation(180)
-    sense.show_message("Hello World!")
 
     def output(self, output_string: str):
-        pass
+        _is_raspberry_pi = False
+
+        try:
+            sense_hat = importlib.import_module('sense_hat')
+            _is_raspberry_pi = True
+        except ModuleNotFoundError:
+            sense_hat = importlib.import_module('sense_emu')
+
+        sense = sense_hat.SenseHat()
+        if _is_raspberry_pi:
+            sense.set_rotation(180)
+        sense.show_message(output_string)
